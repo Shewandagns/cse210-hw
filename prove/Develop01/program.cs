cf578f99c0754336741e7aa3b67125b6fc741c8a
shewandagns <shewandagns@outlook.com>
2024-09-23 16:14:34 +0300

    name game

---
+++ prove/Develop01/program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt for first name
+            Console.Write("What is your first name? ");
+            string firstName = Console.ReadLine();
+
+            // Prompt for last name
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+
+            // Display formatted name
+            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        }
+    }
+
+}
+
